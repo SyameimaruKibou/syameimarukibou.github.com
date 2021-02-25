7b1cccb22a68adca4ad23d1b78cdc91394d1ba7c
kibou-sha <807621745@qq.com>
2021-02-25 11:09:06 +0800

    change travis.yml

--- Update.sh
+++ Update.sh
@@ -1,4 +1,3 @@
+#!/bin/bash
 npm install -g hexo-cli
 npm install
-sed -i'' "s~git@github.com:SyameimaruKibou/syameimarukibou.github.io.git~https://${GIT_REPO_TOKEN}@github.com/SyameimaruKibou/syameimarukibou.github.io.git~" _config.yml
-cat _config.yml
